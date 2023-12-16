bf8e4bd77772ea507964a901fd6504f845df36e7
Tanner <tanner.johnson2718@gmail.com>
2023-12-16 14:16:49 -0900

    ..

--- scripts/esp32_openocd.sh
+++ scripts/esp32_openocd.sh
@@ -3,4 +3,4 @@ if [ $# != 2 ]; then
     exit
 fi
 
-openocd -f ./openocd_config/rpi4_interface.cfg -f target/esp32.cfg -c "adapter speed ${1}" -c "bindto ${2}" -c "esp appimage_offset 0x20000"
+openocd -f ./openocd_config/rpi4_interface.cfg -f target/esp32.cfg -c "adapter speed ${1}" -c "bindto ${2}"  -c "init; halt; esp appimage_offset 0x20000"
